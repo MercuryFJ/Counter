deda70f279647b8d8853c54cbae3c43b41f2ade8
MercuryFJ <alexfj2002@gmail.com>
2024-09-25 13:59:33 +0200

    Date Change and Hour Adjustment
    - An adjustment to the hours has been made because all months are considered to have 30 days.
    - The issue of simultaneous execution of two setInterval functions has been fixed.
    - The code has been optimized and styles have been implemented.

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,25 @@
+let divElement = document.createElement("div");
+divElement.setAttribute("id", "divParent");
+let h1Element = document.createElement("h1");
+h1Element.setAttribute("id", "tittle");
+h1Element.innerText = "Counter";
+let container = document.createElement("div");
+container.setAttribute("id", "divChild");
+let infoDate = document.createElement("p");
+
+infoDate.setAttribute("id", "infoDate");
 let pElement = document.createElement("p");
+pElement.setAttribute("id", "textCounter");
 let inputDate = document.createElement("input");
 inputDate.setAttribute("type", "date");
-inputDate.setAttribute("id", "date");
 
-
-/*function parseDate(date){
-  let arrayDate= date.split("-");
-  let year = arrayDate[0];
-  let month = arrayDate[1]-1; //Se le pone -1 porque los meses van del 0 al 11
-  let day = arrayDate[2];
-  return new Date(year, month, day);
-}*/
-
-function getSeconds(actualDate, targetDate){
+let actualDate = new Date();
+inputDate.setAttribute("value", "2024-10-13");
+getCounter();
+function getCounter(){
+  infoDate.innerText = "Month(s) Day(s) Hour(s) Minute(s) Second(s)";
+  targetDate = new Date(inputDate.value);
+  console.log(inputDate.value);
   let totalSeconds = (targetDate - actualDate) / 1000;
 
   let months = Math.trunc(totalSeconds / 2592000);
@@ -21,7 +28,7 @@ function getSeconds(actualDate, targetDate){
   let days = Math.trunc(totalSeconds / 86400);
   totalSeconds %= 86400;
 
-  let hours = Math.trunc(totalSeconds / 3600);
+  let hours = Math.trunc((totalSeconds-7200) / 3600); //Se restan 2 horas para que el contador cuadre con el horario actual.
   totalSeconds %= 3600;
 
   let minutes = Math.trunc(totalSeconds / 60);
@@ -29,69 +36,62 @@ function getSeconds(actualDate, targetDate){
 
   let seconds = Math.trunc(totalSeconds);
 
-  return [months, days, hours, minutes, seconds];
-}
+  if (window.interval) clearInterval(window.interval);
+  let flag = false;
+  window.interval = setInterval(() => {
 
-let actualDate = new Date();
-inputDate.value = "2024-10-13";
-let targetDate = new Date(inputDate.value);
-let totalMs = targetDate - actualDate;
-let arrayDate = getSeconds(actualDate, targetDate);
-
-let totalSeconds = (targetDate - actualDate) / 1000;
-
-  let months = Math.trunc(totalSeconds / 2592000);
-  totalSeconds %= 2592000;
-
-  let days = Math.trunc(totalSeconds / 86400);
-  totalSeconds %= 86400;
-
-  let hours = Math.trunc(totalSeconds / 3600);
-  totalSeconds %= 3600;
-
-  let minutes = Math.trunc(totalSeconds / 60);
-  totalSeconds %= 60;
-
-  let seconds = Math.trunc(totalSeconds);
-
-  let flag=false;
-setInterval(() => {
-
-  if (seconds < 1) {
-    seconds = 60;
-    minutes--;
-    if (minutes < 0) {
-      minutes = 59;
-      hours--;
-      if (hours < 0) {
-        hours = 23;
-        days--;
-        if (days < 0) {
-          days = 30;
-          months--;
-        } if (months < 0) {
-          flag = true;
+    if (seconds < 1) {
+      seconds = 60;
+      minutes--;
+      if (minutes < 0) {
+        minutes = 59;
+        hours--;
+        if (hours < 0) {
+          hours = 23;
+          days--;
+          if (days < 0) {
+            days = 30;
+            months--;
+          } if (months < 0) {
+            flag = true;
+          }
         }
       }
     }
-  }
 
-  if (!flag) {
-    seconds--;
-    if (months > 1) {
-      pElement.classList = "textGreen";
-    } else if (months < 1) {
-      pElement.classList = "textOrange";
-    } else if (days < 7) {
-      pElement.classList = "textRed";
+    if (!flag) {
+      seconds--;
+      pElement.className = "";
+      infoDate.className = "";
+      if (months >= 1) {
+        pElement.classList.add("textGreen");
+        infoDate.classList.add("textGreen");
+      } else if (months < 1 && days > 6) {
+        pElement.classList.add("textOrange");
+        infoDate.classList.add("textOrange");
+      } else {
+        pElement.classList.add("textRed");
+        infoDate.classList.add("textRed");
+      }
+      pElement.innerText = months + ":" + days + ":" + hours + ":" + minutes + ":" + seconds;
+    } else {
+      infoDate.innerText = "";
+      pElement.className = "";
+      pElement.classList.add("textMagenta");
+      pElement.innerText = "¡¡¡ Feliz Cumpleaños !!!";
+      clearInterval(interval);
     }
-    pElement.innerText = months + ":" + days + ":" + hours + ":" + minutes + ":" + seconds;
-  } else {
-    pElement.innerText = "Feliz Cumpleaños !!!";
-    clearInterval(interval);
-  }
 
-}, 1000);
+  }, 1000);
+}
 
-document.body.append(pElement);
-document.body.append(inputDate);
+inputDate.addEventListener("change", getCounter);
+
+document.body.append(divElement);
+divElement.appendChild(h1Element);
+divElement.appendChild(container);
+container.appendChild(infoDate);
+container.appendChild(pElement);
+container.appendChild(inputDate);
+
+

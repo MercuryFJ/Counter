d690b96eb7981a8e2132fd9057522f07b7fb54ee
MercuryFJ <alexfj2002@gmail.com>
2024-09-26 00:23:12 +0200

    Comments and code structure
    - New commets were added to improve the readability of the project.
    - New code structure.

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,54 @@
+//Father container, this div contains a h1 and child div
 let divElement = document.createElement("div");
 divElement.setAttribute("id", "divParent");
+
+//This h1 has the tittle of the timer
 let h1Element = document.createElement("h1");
 h1Element.setAttribute("id", "tittle");
 h1Element.innerText = "Counter";
+
+//This child div contains the information and the countdown of the timer.
 let container = document.createElement("div");
 container.setAttribute("id", "divChild");
-let infoDate = document.createElement("p");
 
+//infoDate contains the countdown information, that is, the top text of the counter (months, days...)
+let infoDate = document.createElement("p");
 infoDate.setAttribute("id", "infoDate");
+
+//pElement has the timer (Months:days:hours:minutes:seconds)
 let pElement = document.createElement("p");
 pElement.setAttribute("id", "textCounter");
+
+//This input has the posibility of change the target date. The date default is my birthday :)
 let inputDate = document.createElement("input");
 inputDate.setAttribute("type", "date");
-
-let actualDate = new Date();
 inputDate.setAttribute("value", "2024-10-13");
+
+//If we click we can restore the counter and change the target date
+inputDate.addEventListener("change", getCounter);
+
+//This contains today's date.
+let actualDate = new Date();
+
 getCounter();
+
+//Insert all the components in the body element with their childs.
+document.body.append(divElement);
+divElement.appendChild(h1Element);
+divElement.appendChild(container);
+container.appendChild(infoDate);
+container.appendChild(pElement);
+container.appendChild(inputDate);
+
+/**
+ * This function get the counter. 
+ * First part is for compute the total seconds of the current date.
+ * Then set the countdown to avoid negative numbers and print with the correct color.
+ */
 function getCounter(){
   infoDate.innerText = "Month(s) Day(s) Hour(s) Minute(s) Second(s)";
   targetDate = new Date(inputDate.value);
-  console.log(inputDate.value);
+  //Total seconds for the counter.
   let totalSeconds = (targetDate - actualDate) / 1000;
 
   let months = Math.trunc(totalSeconds / 2592000);
@@ -28,7 +57,8 @@ function getCounter(){
   let days = Math.trunc(totalSeconds / 86400);
   totalSeconds %= 86400;
 
-  let hours = Math.trunc((totalSeconds-7200) / 3600); //Se restan 2 horas para que el contador cuadre con el horario actual.
+  //(7200 seconds = 2 hours) are subtracted so that the counter matches the current time .
+  let hours = Math.trunc((totalSeconds-7200) / 3600); 
   totalSeconds %= 3600;
 
   let minutes = Math.trunc(totalSeconds / 60);
@@ -36,8 +66,14 @@ function getCounter(){
 
   let seconds = Math.trunc(totalSeconds);
 
+  //This allows if there is any interval in operation on the page, it can be canceled for correct performance in case the user enters a new date.
   if (window.interval) clearInterval(window.interval);
+
+  //Flag for check if the target date has arrived.
   let flag = false;
+  
+  //window.interval is a global enviroment varible that can do posible the reset of the interval. 
+  //This variable can store the id of setInterval to end the interval if necessary.
   window.interval = setInterval(() => {
 
     if (seconds < 1) {
@@ -78,20 +114,13 @@ function getCounter(){
       infoDate.innerText = "";
       pElement.className = "";
       pElement.classList.add("textMagenta");
-      pElement.innerText = "¡¡¡ Feliz Cumpleaños !!!";
+      pElement.innerText = "Happy Birthday !!!";
       clearInterval(interval);
     }
 
   }, 1000);
 }
 
-inputDate.addEventListener("change", getCounter);
 
-document.body.append(divElement);
-divElement.appendChild(h1Element);
-divElement.appendChild(container);
-container.appendChild(infoDate);
-container.appendChild(pElement);
-container.appendChild(inputDate);
 
 
